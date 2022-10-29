d31b0096a0fe346ef3928565385970dca14a82cb
777AMR <meyram.ramazanov@gmail.com>
2022-10-29 14:58:19 +0600

    Cоздание собственных фильтров

--- app/js/controllers/QuestionController.js
+++ app/js/controllers/QuestionController.js
@@ -1,33 +1,34 @@
-var myApp = angular.module('myApp');
-myApp.controller('QuestionController', function ($scope) {
+questApp.controller('QuestionController',
+    function QuestionController($scope) {
 
-    $scope.sortparam = 'rate';
-    $scope.question = {
-        text: 'Какой js-фреймворк лучше использовать?',
-        author: 'Иван Иванов',
-        date: '20/10/2022',
-        answers:
-            [{
-                text: 'AngularJS!',
-                author: 'Вова Сидоров',
-                date: '20/10/2022',
-                rate: 2
-            }, {
-                text: 'AngularJS лучше всех',
-                author: 'Олег Кузнецов',
-                date: '20/10/2022',
-                rate: 0
-            }, {
-                text: 'Я бы использовал knockout',
-                author: 'Никита Петров',
-                date: '21/10/2022',
-                rate: 0
-            }]
-    };
-    $scope.voteUp = function (answer) {
-        answer.rate++;
-    };
-    $scope.voteDown = function (answer) {
-        answer.rate--;
-    };
-});
+        $scope.question = {
+            text: 'Какой js-фреймворк лучше использовать?',
+            author: 'Иван Иванов',
+            date: '20/10/2022',
+            answers:
+                [{
+                    text: 'AngularJS!',
+                    author: 'Вова Сидоров',
+                    date: '20/10/2022',
+                    rate: 2
+                }, {
+                    text: 'AngularJS лучше всех',
+                    author: 'Олег Кузнецов',
+                    date: '20/10/2022',
+                    rate: 0
+                }, {
+                    text: 'фигасе квестшен',
+                    author: 'Неизвестный',
+                    date: '22/10/2022',
+                    rate: 0
+                }]
+        },
+
+            $scope.voteUp = function (answer) {
+                answer.rate++;
+            };
+        $scope.voteDown = function (answer) {
+            answer.rate--;
+        };
+    }
+)
