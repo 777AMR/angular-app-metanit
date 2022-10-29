62d4038a5ad49e8956a2c95a14eec2d3557f3e38
777AMR <meyram.ramazanov@gmail.com>
2022-10-29 14:45:49 +0600

    Обработка событий

--- app/js/controllers/phonesController.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('myApp');
-myApp.controller('phoneController', function ($scope) {
-    $scope.somestyle = {background: '#eee', color: '#777'};
-    $scope.someclass = "myclass";
-
-    $scope.data = {visible: false};
-});
